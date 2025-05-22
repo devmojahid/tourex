f57345328a748988e90d9b03f5dd8e1b59fe6ae9
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-05-22 11:42:51 +0600

    tour package

--- Cms/themes/theme4/views/index.blade.php
+++ Cms/themes/theme4/views/index.blade.php
@@ -2,4 +2,9 @@
 @section('front-content')
     {{-- hero section --}}
     @include('theme::components.hero')
+
+    {{-- tour package section --}}
+    @include('theme::components.tour-package')
+
+
 @endsection
