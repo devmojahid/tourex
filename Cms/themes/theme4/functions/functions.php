96ee60c73f5e46171753b19b0817c483c8845bed
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-05-22 09:21:21 +0600

    theme 4

---
+++ Cms/themes/theme4/functions/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Theme specific functions for Theme 4
+ */
+
+use Illuminate\Support\Facades\Session;
+
+/**
+ * Get theme information
+ */
+function get_theme_info()
+{
+    return theme()->loadThemeInfo(theme()->current());
+}
+
+/**
+ * Format currency based on site settings
+ */
+function theme4_format_currency($amount)
+{
+    $currency_icon = Session::get('currency_icon', '$');
+    $currency_position = Session::get('currency_position', 'left');
+
+    if ($currency_position == 'left') {
+        return $currency_icon . number_format($amount, 2);
+    } else {
+        return number_format($amount, 2) . $currency_icon;
+    }
+}
+
+/**
+ * Get theme version
+ */
+function theme4_get_version()
+{
+    return '1.0.0';
+}
