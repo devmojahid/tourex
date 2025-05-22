836516d29cb356a447d043180858003337d581b2
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-05-22 16:22:29 +0600

    Destination

--- Cms/themes/theme5/views/index.blade.php
+++ Cms/themes/theme5/views/index.blade.php
@@ -1,6 +1,10 @@
 @extends('theme::layouts.app')
 @section('front-content')
+
     {{-- hero section --}}
     @include('theme::components.hero')
+
+    {{-- destination section --}}
+    @include('theme::components.destination')
 
 @endsection
