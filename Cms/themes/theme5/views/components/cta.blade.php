8ff474e97b3754ddbb905bddd1087a46c83b2763
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-05-22 20:41:41 +0600

    cta

--- Cms/themes/theme5/views/components/cta.blade.php
+++ Cms/themes/theme5/views/components/cta.blade.php
@@ -1,36 +1,41 @@
 @php
-    $theme4_cta = getContent('theme4_cta.content', true);
+    $theme5_cta = getContent('theme5_cta.content', true);
 @endphp
 
-@if (!empty($theme4_cta))
+@if (!empty($theme5_cta))
     <!-- tg-cta-area-start -->
-    <div class="tg-cta-area-area tg-cta-su-2 tg-primary-bg p-relative z-index-1">
-        <img class="shape-2 p-absolute d-none d-xl-block" src="assets/img/cta/su/shape-3.png" alt="">
+    <div class="tg-cta-area-area tg-primary-bg p-relative z-index-1">
+        <img class="tg-cta-3-shape-2 d-none d-md-block" src="{{ asset('frontend/assets/img/shape/stadium-2.png') }}" alt="">
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-6 col-md-6">
-                    <div class="tg-cta-3-content tg-process-content pt-50 pb-50">
-                        <h5 class="tg-section-su-subtitle su-subtitle-2 mb-15">
-                            {{ getTranslatedValue($theme4_cta, 'sub_title') }}</h5>
-                        <h2 class="tg-section-su-title text-capitalize mb-25">
-                            {!! strip_tags(clean(getTranslatedValue($theme4_cta, 'title')), '<br>') !!}
-                        </h2>
-                        <div class="tg-cta-3-apps d-flex align-items-center">
-                            <a class="d-inline-block mr-10"
-                                href="{{ getTranslatedValue($theme4_cta, 'google_play_link') }}">
-                                <img src="{{ asset('frontend/assets/img/shape/google.png') }}" alt=""></a>
-
-                            <a class="d-inline-block" href="{{ getTranslatedValue($theme4_cta, 'apple_store_link') }}">
-                                <img src="{{ asset('frontend/assets/img/shape/app.png') }}" alt="">
-                            </a>
-                        </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="tg-cta-3-thumb p-relative z-index-1  pt-85 ">
+                        <img class="w-100" src="{{ asset(getSingleImage($theme5_cta, 'image')) }}" alt="">
+                        <img class="tg-cta-3-shape rotate-infinite" src="{{ asset('frontend/assets/img/shape/star-6.png') }}" alt="shape">
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6">
-                    <div class="tg-cta-3-thumb p-relative z-index-1 text-end pt-90">
-                        <img src="{{ asset(getSingleImage($theme4_cta, 'image')) }}" alt="">
-                        <img class="shape p-absolute d-none d-xl-block" src="assets/img/cta/su/shape.png"
-                            alt="">
+                <div class="col-lg-5 col-md-6">
+                    <div class="tg-cta-3-content mb-20 pt-15">
+                        <h2 class="mb-15 tg-cta-title text-white text-capitalize mb-5">
+                            {!! strip_tags(clean(getTranslatedValue($theme5_cta, 'title')), '<br>') !!}
+                        </h2>
+                        <p class="text-white">
+                            {{ getTranslatedValue($theme5_cta, 'sub_title') }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="tg-cta-3-apps pt-15 d-flex align-items-center">
+
+                        <a class="mb-10 d-inline-block mr-5"
+                            href="{{ getTranslatedValue($theme5_cta, 'google_play_link') }}">
+                            <img src="{{ asset('frontend/assets/img/shape/google.png') }}" alt=""></a>
+
+                        <a class="mb-10 d-inline-block"
+                            href="{{ getTranslatedValue($theme5_cta, 'apple_store_link') }}">
+                            <img src="{{ asset('frontend/assets/img/shape/app.png') }}" alt="">
+                        </a>
+
                     </div>
                 </div>
             </div>
--- Cms/themes/theme5/views/index.blade.php
+++ Cms/themes/theme5/views/index.blade.php
@@ -28,4 +28,7 @@
     {{-- blog section --}}
     @include('theme::components.blog')
 
+    {{-- cta section --}}
+    @include('theme::components.cta')
+
 @endsection
