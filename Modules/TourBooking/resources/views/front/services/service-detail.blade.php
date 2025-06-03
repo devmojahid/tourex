05be9d7d91d64486cd1c419c6872171cbf533da9
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-06-03 17:03:48 +0600

    Update service-detail.blade.php

--- Modules/TourBooking/resources/views/front/services/service-detail.blade.php
+++ Modules/TourBooking/resources/views/front/services/service-detail.blade.php
@@ -366,9 +366,10 @@
                                                                             </div>
                                                                         @endif
 
-                                                                         @if ($itinerary?->duration)
+                                                                        @if ($itinerary?->duration)
                                                                             <div class="mt-3">
-                                                                                <strong><i class="fa-solid fa-business-time"></i>
+                                                                                <strong><i
+                                                                                        class="fa-solid fa-business-time"></i>
                                                                                     Duration:</strong>
                                                                                 {{ $itinerary?->duration }}
                                                                             </div>
@@ -400,9 +401,10 @@
                                             ws you to check off a range of southern England‘s are historical.</p>
                                         <div class="tg-tour-about-map h-100">
                                             <iframe
-                                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31078.361591144112!2d-74.0256365664179!3d40.705584751235754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1724572184688!5m2!1sen!2sbd"
+                                                src="https://www.google.com/maps/embed/v1/view?key={{ env('GOOGLE_MAPS_API_KEY') }}&center={{ $service?->latitude }},{{ $service?->longitude }}&zoom=13"
                                                 width="600" height="450" style="border:0;" allowfullscreen=""
-                                                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                                loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                            </iframe>
                                         </div>
                                     </div>
                                     <div class="tg-tour-about-border mb-45"></div>
