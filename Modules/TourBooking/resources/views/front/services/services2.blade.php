7588d978d0f534b90767eb6f2f0e4289457503a6
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-06-05 18:07:26 +0600

    service 2 ratting and preloader

--- Modules/TourBooking/resources/views/front/services/services2.blade.php
+++ Modules/TourBooking/resources/views/front/services/services2.blade.php
@@ -380,70 +380,30 @@
                                     <span class="tg-filter-border mt-25 mb-25"></span>
                                 </div>
 
-
                                 <h4 class="tg-filter-title mb-15">Top Reviews</h4>
                                 <div class="tg-filter-list">
                                     <ul>
-                                        <li>
-                                            <div class="checkbox d-flex">
-                                                <input class="tg-checkbox" type="checkbox">
-                                                <div class="tg-filter-review">
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
+                                        @for ($i = 5; $i >= 1; $i--)
+                                            <li>
+                                                <div class="checkbox d-flex">
+                                                    <input id="rating_{{ $i }}" x-model="filters.ratings"
+                                                        class="tg-checkbox" type="checkbox" value="{{ $i }}"
+                                                        name="filter_ratings[]">
+                                                    <div class="tg-filter-review">
+                                                        <label for="rating_{{ $i }}">
+                                                            @for ($j = 1; $j <= 5; $j++)
+                                                                @if ($j <= $i)
+                                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
+                                                                @else
+                                                                    <span class="bad-review"><i
+                                                                            class="fa-light fa-star-sharp"></i></span>
+                                                                @endif
+                                                            @endfor
+                                                        </label>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="checkbox d-flex">
-                                                <input class="tg-checkbox" type="checkbox">
-                                                <div class="tg-filter-review">
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="checkbox d-flex">
-                                                <input class="tg-checkbox" type="checkbox">
-                                                <div class="tg-filter-review">
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="checkbox d-flex">
-                                                <input class="tg-checkbox" type="checkbox">
-                                                <div class="tg-filter-review">
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="checkbox d-flex">
-                                                <input class="tg-checkbox" type="checkbox">
-                                                <div class="tg-filter-review">
-                                                    <span><i class="fa-solid fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                    <span class="bad-review"><i class="fa-light fa-star-sharp"></i></span>
-                                                </div>
-                                            </div>
-                                        </li>
+                                            </li>
+                                        @endfor
                                     </ul>
                                 </div>
                                 <span class="tg-filter-border mt-25 mb-25"></span>
@@ -557,11 +517,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div id="filter_data">
-                                <div id="loading item_loading">
-                                    <div class="loader"></div>
-                                </div>
-                            </div>
+                            <div id="filter_data"></div>
                         </div>
                     </div>
                 </div>
@@ -590,6 +546,8 @@
     </script>
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js">
+    </script>
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.data('data', () => ({
@@ -651,6 +609,7 @@
                     amenity_ids: {!! json_encode(request('amenity_ids', [])) !!},
                     languages: {!! json_encode(request('languages', [])) !!},
                     sort_by: `{{ request('sort_by', '') }}`,
+                    ratings: {!! json_encode(request('ratings', [])) !!},
                 },
                 defaultFilters: {
                     search: '',
@@ -660,6 +619,7 @@
                     amenity_ids: [],
                     languages: [],
                     sort_by: '',
+                    ratings: []
                 },
                 get isFilterChanged() {
                     return JSON.stringify(this.filters) !== JSON.stringify(this.defaultFilters);
@@ -731,6 +691,9 @@
                 },
                 fetchServices() {
                     that = this;
+
+                    this.loadingOverlay("show");
+
                     $.ajax({
                         url: `{{ route('front.tourbooking.services.load.ajax') }}`,
                         method: 'GET',
@@ -741,11 +704,6 @@
                             isListView: this.isListView,
                             style: this.style
                         },
-                        beforeSend: function() {
-                            $('#filter_data').html(
-                                `<div id="loading item_loading"><div class="loader"></div></div>`
-                            );
-                        },
                         success: function(response) {
                             $('#filter_data').html(response.view);
                             $('.custom_pagination_count').html(response
@@ -753,6 +711,9 @@
                         },
                         error: function(xhr, status, error) {
                             console.error(error);
+                        },
+                        complete: function() {
+                            that.loadingOverlay("hide");
                         }
                     });
                 },
@@ -785,7 +746,23 @@
 
                     });
 
+                    this.loadingOverlay("show");
+
                     this.fetchServices();
+                },
+
+                loadingOverlay(action = 'show', target = false) {
+                    const options = {
+                        size: 50,
+                        maxSize: 50,
+                        minSize: 50
+                    };
+
+                    if (target && typeof target === 'string') {
+                        $(target).LoadingOverlay(action, options);
+                    } else {
+                        $.LoadingOverlay(action, options);
+                    }
                 }
             }));
         });
