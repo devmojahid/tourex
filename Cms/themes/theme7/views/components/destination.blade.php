7729a85438a9162d785a903d7abae66613eae30c
Md Nayem Ali <106856439+devnayemali@users.noreply.github.com>
2025-06-21 19:04:32 +0600

    Update destination.blade.php

--- Cms/themes/theme7/views/components/destination.blade.php
+++ Cms/themes/theme7/views/components/destination.blade.php
@@ -1,10 +1,12 @@
 @php
     $theme7_destination = getContent('theme7_destination.content', true);
+    $home7_destination_items = popularDestinations(8, false);
 @endphp
 
 <!-- tg-location-area-start -->
 <div class="tg-location-area pt-135 pb-130 tg-grey-bg p-relative z-index-1">
-    <img class="tg-location-shape d-none d-xl-block" src="{{ asset('frontend/assets/img/shape/tower.png') }}" alt="">
+    <img class="tg-location-shape d-none d-xl-block" src="{{ asset('frontend/assets/img/shape/tower.png') }}"
+        alt="">
     <div class="container">
         <div class="row align-items-end">
             <div class="col-lg-9">
@@ -28,89 +30,54 @@
                 </div>
             @endif
         </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="swiper-container tg-location-5-slider p-relative fix">
-                    <div class="swiper-wrapper">
-                        <div class="swiper-slide">
-                            <div class="tg-location-5-wrap p-relative">
-                                <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
-                                    <img class="w-100" src="assets/img/location/location-5/location-3.jpg"
-                                        alt="location">
-                                    <span class="tg-location-5-bottom-bg"></span>
-                                    <div class="tg-location-5-inner p-absolute">
-                                        <div class="tg-location-5-content text-center">
-                                            <h4 class="mb-0 lh-1"><a href="map-listing.html">New york City </a></h4>
-                                            <span>05 Restaurants</span>
+        @if ($home7_destination_items != null && $home7_destination_items->count() > 0)
+            <div class="row">
+                <div class="col-12">
+                    <div class="swiper-container tg-location-5-slider p-relative fix">
+                        <div class="swiper-wrapper">
+                            @foreach ($home7_destination_items as $key => $destination_item)
+                                <div class="swiper-slide">
+                                    <div class="tg-location-5-wrap p-relative">
+                                        <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
+
+                                            <img class="w-100"
+                                                src="{{ asset($destination_item->image ?? 'frontend/assets/img/shape/placeholder.png') }}"
+                                                alt="{{ $destination_item->name }}">
+
+                                            <span class="tg-location-5-bottom-bg"></span>
+                                            <div class="tg-location-5-inner p-absolute">
+                                                <div class="tg-location-5-content text-center">
+                                                    <h4 class="mb-0 lh-1">
+                                                        <a
+                                                            href="{{ route('front.tourbooking.services', ['destination_id' => $destination_item->id, 'destination' => $destination_item->name]) }}">
+                                                            {{ $destination_item->name }}
+                                                        </a>
+                                                    </h4>
+                                                    <span>
+                                                        {{ $destination_item->services_count }}
+                                                        {{ $destination_item->services_count > 1 ? __('translate.Tours') : __('translate.Tour') }}
+                                                    </span>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="tg-location-5-wrap p-relative">
-                                <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
-                                    <img class="w-100" src="assets/img/location/location-5/location-2.jpg"
-                                        alt="location">
-                                    <span class="tg-location-5-bottom-bg"></span>
-                                    <div class="tg-location-5-inner p-absolute">
-                                        <div class="tg-location-5-content text-center">
-                                            <h4 class="mb-0 lh-1"><a href="map-listing.html">Australia</a></h4>
-                                            <span>07 Restaurants</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="tg-location-5-wrap p-relative">
-                                <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
-                                    <img class="w-100" src="assets/img/location/location-5/location.jpg"
-                                        alt="location">
-                                    <span class="tg-location-5-bottom-bg"></span>
-                                    <div class="tg-location-5-inner p-absolute">
-                                        <div class="tg-location-5-content text-center">
-                                            <h4 class="mb-0 lh-1"><a href="map-listing.html">Switzerland</a></h4>
-                                            <span>12 Restaurants</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="tg-location-5-wrap p-relative">
-                                <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
-                                    <img class="w-100" src="assets/img/location/location-5/location-4.jpg"
-                                        alt="location">
-                                    <span class="tg-location-5-bottom-bg"></span>
-                                    <div class="tg-location-5-inner p-absolute">
-                                        <div class="tg-location-5-content text-center">
-                                            <h4 class="mb-0 lh-1"><a href="map-listing.html">Japan City</a></h4>
-                                            <span>03 Restaurants</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide">
-                            <div class="tg-location-5-wrap p-relative">
-                                <div class="tg-location-5-thumb bg-white p-relative rounded-circale">
-                                    <img class="w-100" src="assets/img/location/location-5/location.jpg"
-                                        alt="location">
-                                    <span class="tg-location-5-bottom-bg"></span>
-                                    <div class="tg-location-5-inner p-absolute">
-                                        <div class="tg-location-5-content text-center">
-                                            <h4 class="mb-0 lh-1"><a href="map-listing.html">Switzerland</a></h4>
-                                            <span>12 Restaurants</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     </div>
 </div>
 <!-- tg-location-area-end -->
+
+@push('style_section')
+    <style>
+        .tg-location-5-thumb {
+            
+            height: 270px;
+            width: 270px;
+        }
+    </style>
+@endpush
