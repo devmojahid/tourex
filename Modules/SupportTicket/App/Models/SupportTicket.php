4d16a354299a7bd2167fe44d896ae6d72c45df02
Mojahid Islam <raofahmedmojahid@gmail.com>
2025-06-25 21:06:45 +0600

    course modules removed

--- Modules/SupportTicket/App/Models/SupportTicket.php
+++ Modules/SupportTicket/App/Models/SupportTicket.php
@@ -3,7 +3,6 @@
 namespace Modules\SupportTicket\App\Models;
 
 use App\Models\User;
-use Modules\Course\App\Models\Course;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -20,10 +19,4 @@ class SupportTicket extends Model
     public function author(){
         return $this->belongsTo(User::class, 'author_id')->select('id', 'name', 'email', 'phone');
     }
-
-    public function course(){
-        return $this->belongsTo(Course::class, 'course_id');;
-    }
-
-
 }
